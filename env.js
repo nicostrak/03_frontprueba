541200ee7fb7b1c8b43585d6a98f5f83ab5625a8
nicostrak <nicostrak@users.noreply.github.com>
2025-12-18 03:32:19 +0000

    deploy: 880c6e49a041d75f300d31e0875443f89c4bfe21

---
+++ env.js
@@ -0,0 +1,5 @@
+// Por favor, reemplaza los siguientes valores con tus credenciales de Supabase
+// Puedes encontrarlas en tu proyecto de Supabase > Settings > API
+
+const SUPABASE_URL = "https://lzhwfchgrhaoxmgemdto.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6aHdmY2hncmhhb3htZ2VtZHRvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MjY4NjA0OSwiZXhwIjoyMDY4MjYyMDQ5fQ.3wZyDJ9Ifb6kdGezmrR_HvNs0nSSUJT3nfNGHaZK1Ns";
